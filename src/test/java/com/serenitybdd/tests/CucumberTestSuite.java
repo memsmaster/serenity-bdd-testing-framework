dca6f83f302fa99fd7997b9756f022fd82374157
m.emin <mamuti@proton.me>
2024-07-16 00:22:43 +0300

    README modified

--- src/test/java/com/serenitybdd/tests/CucumberTestSuite.java
+++ src/test/java/com/serenitybdd/tests/CucumberTestSuite.java
@@ -1,9 +1,9 @@
 //package com.serenitybdd.tests;
-import org.junit.platform.suite.api.ConfigurationParameter;
-import org.junit.platform.suite.api.IncludeEngines;
-import org.junit.platform.suite.api.SelectClasspathResource;
-import org.junit.platform.suite.api.Suite;
-import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+//import org.junit.platform.suite.api.ConfigurationParameter;
+//import org.junit.platform.suite.api.IncludeEngines;
+//import org.junit.platform.suite.api.SelectClasspathResource;
+//import org.junit.platform.suite.api.Suite;
+//import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 //@Suite
 //@IncludeEngines("cucumber")
